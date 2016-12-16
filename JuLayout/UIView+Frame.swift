363488759f06ef3916e5f2a16f22288fc2b6222d
juvid <jutewei@qq.com>
2016-12-16 20:18:41 +0800

    设置frame

--- JuLayout/UIView+Frame.swift
+++ JuLayout/UIView+Frame.swift
@@ -8,5 +8,52 @@
 
 import UIKit
 extension UIView{
-   
+    var originX :CGFloat?{
+        get{
+            return self.frame.origin.x
+        }
+        set{
+            var frame = self.frame
+            frame.origin.x = newValue!
+            self.frame = frame;
+        }
+    }
+    var originY :CGFloat?{
+         get{
+        return self.frame.origin.y
+        }
+        set{
+            var frame = self.frame
+            frame.origin.y = newValue!
+            self.frame = frame;
+        }
+    }
+    var sizeW :CGFloat?{
+        get{
+            return self.frame.size.width
+        }
+        set{
+            var frame = self.frame
+            frame.size.width = newValue!
+            self.frame = frame;
+
+        }
+    }
+    var sizeH :CGFloat?{
+        get{
+            return self.frame.size.height
+        }
+        set{
+            var frame = self.frame
+            frame.size.height = newValue!
+            self.frame = frame;
+
+        }
+    }
+    func removeAllSubviews() {
+        while (self.subviews.count > 0) {
+            let subView = self.subviews.first
+            subView?.removeFromSuperview()
+        }
+    }
 }
